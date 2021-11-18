fbea21ddb68569fb86adeac11185bd7e698c315b
bbv888 <vladtudose2003@yahoo.com>
2021-11-19 00:48:52 +0100

    added PUT for AttendeeController

--- src/main/java/com/bbv/boot/conference/controller/AttendeeController.java
+++ src/main/java/com/bbv/boot/conference/controller/AttendeeController.java
@@ -2,7 +2,9 @@ package com.bbv.boot.conference.controller;
 
 import com.bbv.boot.conference.model.Attendee;
 import com.bbv.boot.conference.repository.AttendeeRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +27,13 @@ public class AttendeeController {
     @PostMapping("/api/v1/attendee")
     public Attendee createAttendee(@RequestBody Attendee attendee){
         return attendeeRepository.saveAndFlush(attendee);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/api/v1/attendee/{id}")
+    public ResponseEntity<Attendee> updateAttendee(@PathVariable Long id, @RequestBody Attendee attendee) {
+        Attendee existingAttendee= attendeeRepository.findById(id).get();
+        BeanUtils.copyProperties(attendee, existingAttendee, "id");
+        attendeeRepository.saveAndFlush(existingAttendee);
+        return ResponseEntity.ok(existingAttendee);
     }
 }
